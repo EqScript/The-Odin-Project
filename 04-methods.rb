3d73f3d3f891b880e9a29e82c2287006a2d4fabe
Sergio Belenkoff <EqScript@users.noreply.github.com>
2024-04-16 09:58:28 +0700

    Methods - done!

--- 04-methods.rb
+++ 04-methods.rb
@@ -7,3 +7,13 @@ puts greet("Carol")
 
 phrase = %w[amazing is ruby that know you did]
 puts phrase.reverse.join(" ").capitalize
+
+def is_between_minus1_100 (number = 88)
+  number.between?(-1, 100)
+end
+
+if is_between_minus1_100()
+  puts "Yes!"
+else
+  puts "No!"
+end
