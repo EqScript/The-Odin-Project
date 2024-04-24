bd6990da574a6680d4f6ef2e9740175195f56f8d
Sergio Belenkoff <EqScript@users.noreply.github.com>
2024-04-24 22:08:31 +0700

    Caesar_sipher exercise

---
+++ 08-caezar_cipher.rb
@@ -0,0 +1,33 @@
+LOWCASE_RANGE = ('a'.ord .. 'z'.ord)
+UPCASE_RANGE = ('A'.ord .. 'Z'.ord)
+
+def shift_letter(char, step)
+  char_code = char.ord
+  if LOWCASE_RANGE.include?(char_code)
+    new_char_code = (char_code - LOWCASE_RANGE.first + step) % 26 + LOWCASE_RANGE.first
+    new_char_code.chr
+  elsif UPCASE_RANGE.include?(char_code)
+    new_char_code = (char_code - UPCASE_RANGE.first + step) % 26 + UPCASE_RANGE.first
+    new_char_code.chr
+  end
+end
+
+def symbol_shift(original_text, step)
+  split_arr = original_text.split('')
+  split_arr.map! { |letter| shift_letter(letter, step) }
+  split_arr.join
+end
+
+loop do
+  puts 'Input the string (empty to exit): '
+  text_to_encrypt = gets.chomp
+
+  if text_to_encrypt.empty?
+    puts 'Now exit...'
+    break
+  end
+
+  puts 'Enter key: '
+  step = gets.chomp.to_i
+  p symbol_shift(text_to_encrypt, step)
+end
